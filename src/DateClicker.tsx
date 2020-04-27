74e1edad3f396f8f833d5bf8aac529e0fd5de7b3
Oded Welgreen <oded.welgreen@gmail.com>
2020-04-27 17:12:19 -0500

    use string instead of date

--- src/DateClicker.tsx
+++ src/DateClicker.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 const DateClicker = () => {
-  const [lastClicked, setLastClicked] = React.useState<Date>(new Date());
+  const [lastClicked, setLastClicked] = React.useState<string>(
+    new Date().toLocaleTimeString()
+  );
   const onClick = () => {
-    setLastClicked(new Date());
+    setLastClicked(new Date().toLocaleTimeString());
   };
   return (
     <div className="date-clicker">
       <button className="btn" onClick={onClick}>
         Click Me
       </button>
-      <div className="click-date">{lastClicked.toLocaleTimeString()}</div>
+      <div className="click-date">{lastClicked}</div>
     </div>
   );
 };
